525c0cb8b8282cb3944898a4916211246d21f9e3
Wenhui Zho <wenhuizho@yahoo.com>
2024-11-04 19:47:46 -0800

    fix

--- backend/app/services/ec2_service.py
+++ backend/app/services/ec2_service.py
@@ -115,7 +115,8 @@ cat <<'EOF' > /home/ubuntu/process_job.py
 {init_script}
 EOF
 # Run the script with parameter
-sudo DB_PASSWORD="$DB_USER_PASSWORD" AWS_SECRET_ACCESS_KEY="$S3_AWS_ACCESS_KEY" -u ubuntu python3 /home/ubuntu/process_job.py "{script_param}"
+su - ubuntu -c "DB_PASSWORD=$DB_USER_PASSWORD AWS_SECRET_ACCESS_KEY=$S3_AWS_ACCESS_KEY python3 /home/ubuntu/process_job.py {script_param} &> out"
+#sudo DB_PASSWORD="$DB_USER_PASSWORD" AWS_SECRET_ACCESS_KEY="$S3_AWS_ACCESS_KEY" -u ubuntu python3 /home/ubuntu/process_job.py "{script_param}"
 """
 
     instance_id = request_spot_instance(region, instance_type, max_price, user_data, key_name, security_group_id, iam_instance_profile_name)
--- backend/app/services/remote_service.py
+++ backend/app/services/remote_service.py
@@ -78,7 +78,7 @@ def run_remote(job_id):
     db_user_password = db_user_password_param["Parameter"]["Value"]
     aws_access_key_param = ssm_client.get_parameter(Name='s3_aws_access_key')
     aws_access_key = aws_access_key_param["Parameter"]["Value"]
-    job_command = f"su - ubuntu -c 'DB_PASSWORD={db_user_password} AWS_SECRET_ACCESS_KEY={aws_access_key} python3 /home/ubuntu/process_job.py {job_id} &> out"
+    job_command = f"su - ubuntu -c 'DB_PASSWORD={db_user_password} AWS_SECRET_ACCESS_KEY={aws_access_key} python3 /home/ubuntu/process_job.py {job_id} &> out'"
     run_command_ssm(ec2_instance.region, [ec2_instance.instance_id], job_command)
     return
   
